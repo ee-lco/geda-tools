b4061098c2f341b5ee85463c595bd8d1e169d1b1
eelco <eelco@98decd40-a97c-4bc3-bd75-91d7e82d8eb1>
2013-09-22 18:25:17 +0000

    changed varmap.scm to read from stdin and added support for target-attribs
    git-svn-id: https://svn.code.sf.net/p/eelco/things/tools/trunk@15 98decd40-a97c-4bc3-bd75-91d7e82d8eb1

--- mipmap/mipmap/gaf-utils.scm
+++ mipmap/mipmap/gaf-utils.scm
@@ -5,26 +5,40 @@
   #:use-module (ice-9 rdelim)
   #:export (load-page! page-save! page-save-as!)
   #:export (object-filter-attribs)
-  #:export (component-add-attrib component-del-attrib))
+  #:export (component-add-attrib component-del-attrib)
+  #:export (remove-attrib!))
 
+; Creates a gEDA <page> from the file <filename>
 (define (load-page! filename)
   (string->page filename (read-delimited "" (open-input-file filename))))
+
+; Saves <page> to the file specified by its associated <filename>
 (define (page-save! page)
   (display (page->string page) (open-output-file (page-filename page))))
+
+; Saves <page> to the file <filename>
 (define (page-save-as! page filename)
   (display (page->string page) (open-output-file filename)))
 
-(define (object-filter-attribs component name)
-  (filter
-    (lambda (attrib) (string=? (attrib-name attrib) name))
-    (object-attribs component)))
+; Returns a list of all attributes attached to <object> that have
+; attrib-name <name> and (optionally) attrib-value <value=opts[0]>.
+(define (object-filter-attribs object name . opts)
+  (let ((value (if (null? opts) #f (car opts))))
+    (filter
+      (lambda (attrib)
+        (and
+          (string=? (attrib-name attrib) name)
+          (if (null? value) #t (string=? (attrib-value attrib) value))))
+      (object-attribs object))))
 
+; Deprecated
 (define (component-add-attrib page component devmap-attrib attrib-string)
   (let ((attrib (apply make-text (text-info devmap-attrib))))
     (set-text-string! attrib attrib-string)
     (page-append! page attrib)
     (attach-attribs! component attrib)))
 
+; Deprecated
 (define (component-del-attrib page component devmap-attrib attrib-string)
   (map
     (lambda (attrib)
@@ -34,4 +48,11 @@
       (lambda (attrib)
         (string=? (attrib-name attrib) attrib-string))
       (object-attribs component))))
+
+; Removes <attrib> from its associated page.
+(define (remove-attrib! attrib)
+  (let ((component (attrib-attachment attrib))
+        (page (object-page attrib)))
+    (if component (detach-attribs! component attrib))
+    (if page (page-remove! page attrib))))
 
--- mipmap/varmap.scm
+++ mipmap/varmap.scm
@@ -3,37 +3,52 @@
   (geda object)
   (geda attrib)
   (ice-9 getopt-long)
+  (ice-9 rdelim)
   (ice-9 regex)
   (srfi srfi-1)
   (mipmap gaf-utils)
   (mipmap getopt-utils))
 
-(define pages #f)
-(define variant #f)
+; Performs varmapping on the specified attrib <var-attrib>
+(define (process-attrib component var-attrib variant)
+  ; Check if the attrib-value matches the var-attrib format:
+  ; [<var-spec>] <new-attrib-value>
+  ; with <var-spec> being a comma separated list of variants
+  (let ((match (string-match "^[[]([^]]*)[]][[:space:]]*(.*)$" (attrib-value var-attrib))))
+    (if match
+      ; Check if <var-spec> contains <variant>
+      (if (find (lambda (var) (string=? var variant)) (string-split (match:substring match 1) #\,))
+        (begin
+          ; Check if a <target-attrib> exists. If so, assign <new-attrib-value> to <target-attrib>
+          ; and delete <var-attrib>, otherwise assign <new-attrib-value> to <var-attrib>
+          ; A <target-attrib> should have the same attrib-name as <var-attrib> and
+          ; attrib-value "*"
+          (let ((target-attrib (object-filter-attribs component (attrib-name var-attrib) "*")))
+            (if (null? target-attrib)
+              (set-attrib-value! var-attrib (match:substring match 2))
+              (begin
+                (set-attrib-value! (car target-attrib) (match:substring match 2))
+                (remove-attrib! var-attrib)))))
+        ; No match, delete <var-attrib>
+        (remove-attrib! var-attrib)))))
 
-(define (process-page page-name)
-  (let ((page (load-page! page-name)))
-    (map
-      (lambda (component)
-        (map
-          (lambda (attrib)
-            (let ((match (string-match "^[[]([^]]*)[]][[:space:]]*(.*)$" (attrib-value attrib))))
-              (if match
-                (if (find (lambda (var) (string=? var variant)) (string-split (match:substring match 1) #\,))
-                  (set-attrib-value! attrib (match:substring match 2))
-                  (begin
-                    (detach-attribs! component attrib)
-                    (page-remove! page attrib))))))
-          (object-attribs component)))
-      (filter component? (page-contents page)))
-    (page-save! page)))
+; Performs varmapping on all component attributes on <page>
+(define (process-page page variant)
+  (map
+    (lambda (component)
+      (map 
+        (lambda (var-attrib) (process-attrib component var-attrib variant))
+        (object-attribs component)))
+    (filter component? (page-contents page))))
 
+; @todo simplify argument handling, only one argument <variant> is needed.
 (define (main args)
   (let* ((option-spec '((variant (single-char #\v) (value #t))))
-         (options (getopt-long args option-spec)))
-    (set! variant (option-ref options 'variant "ALL"))
-    (let ((pages (option-ref options '() '())))
-      (map process-page pages))))
+         (options (getopt-long args option-spec))
+         (variant (option-ref options 'variant "ALL"))
+         (page (string->page "STDIN" (read-delimited "" (current-input-port)))))
+    (process-page page variant)
+    (display (page->string page))))
 
 (main (command-line))
 
