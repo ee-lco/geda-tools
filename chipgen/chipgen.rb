f70b1ff73c15af5ac23360156b14f8a7d91b852a
Eelco van der Werff <eelco@users.sourceforge.net>
2015-06-07 21:11:56 +0200

    chipgen.rb: add support for sheet symbols

--- chipgen/chipgen.rb
+++ chipgen/chipgen.rb
@@ -18,7 +18,7 @@ class Pin
             puts "pintype=%s" % [@type]
             puts "T %d %d 5 16 0 0 0 0 1" % [x - 200, y]
             puts "pinseq=%s" % [@num]
-            puts "T %d %d 5 16 1 1 0 6 1" % [x - 100, y + 50]
+            puts "T %d %d 5 16 %d %d 0 6 1" % [x - 100, y + 50, $sheet ? 0 : 1, $sheet ? 0 : 1]
             puts "pinnumber=%s" % [@num]
             puts "T %d %d 9 16 1 1 0 0 1" % [x +  50, y]
             puts "pinlabel=%s" % [@label]
@@ -34,7 +34,7 @@ class Pin
             puts "pintype=%s" % [@type]
             puts "T %d %d 5 16 0 0 0 6 1" % [x + 200, y]
             puts "pinseq=%s" % [@num]
-            puts "T %d %d 5 16 1 1 0 0 1" % [x + 100, y + 50]
+            puts "T %d %d 5 16 %d %d 0 0 1" % [x + 100, y + 50, $sheet ? 0 : 1, $sheet ? 0 : 1]
             puts "pinnumber=%s" % [@num]
             puts "T %d %d 9 16 1 1 0 6 1" % [x -  50, y]
             puts "pinlabel=%s" % [@label]
@@ -50,7 +50,7 @@ class Pin
             puts "pintype=%s" % [@type]
             puts "T %d %d 5 16 0 0 90 0 1" % [x, y + 200, y]
             puts "pinseq=%s" % [@num]
-            puts "T %d %d 5 16 1 1 0 0 1" % [x + 100, y + 50]
+            puts "T %d %d 5 16 %d %d 0 0 1" % [x + 100, y + 50, $sheet ? 0 : 1, $sheet ? 0 : 1]
             puts "pinnumber=%s" % [@num]
             puts "T %d %d 9 16 1 1 90 6 1" % [x, y - 50]
             puts "pinlabel=%s" % [@label]
@@ -66,7 +66,7 @@ class Pin
             puts "pintype=%s" % [@type]
             puts "T %d %d 5 16 0 0 90 0 1" % [x, y - 200, y]
             puts "pinseq=%s" % [@num]
-            puts "T %d %d 5 16 1 1 0 2 1" % [x + 100, y - 50]
+            puts "T %d %d 5 16 %d %d 0 2 1" % [x + 100, y - 50, $sheet ? 0 : 1, $sheet ? 0 : 1]
             puts "pinnumber=%s" % [@num]
             puts "T %d %d 9 16 1 1 90 0 1" % [x, y + 50]
             puts "pinlabel=%s" % [@label]
@@ -81,10 +81,16 @@ end
 
 puts "v 20121203 2"
 
+$sheet = false
 pins = { "l" => [], "r" => [], "t" => [], "b" => [] }
 $stdin.each_line do |line|
-    pin = Pin.new(*line.chomp.split(","))
-    pins[pin.dir].push(pin)
+    line = line.chomp
+    if line == "sheet=1" then
+        $sheet = true
+    else
+        pin = Pin.new(*line.split(","))
+        pins[pin.dir].push(pin)
+    end
 end
 #pins.each { |dir| dir.sort! { |a,b| a.pinseq <=> b.pinseq } }
 vlen = [pins["l"].length, pins["r"].length, 4].max
@@ -94,9 +100,18 @@ pins["r"].each_index { |i| pins["r"][i].print(1000 + (hlen + 1) * 1000, 1000 + (
 pins["t"].each_index { |i| pins["t"][i].print(2000 + i * 1000, 1000 + (vlen + 1) * 1000) }
 pins["b"].each_index { |i| pins["b"][i].print(2000 + i * 1000, 1000) }
 
-puts "T %d %d 8 32 1 1 0 4 1" % [1000 + (hlen + 1) * 500, 1500 + (vlen + 1) * 500]
-puts "device=DEVICE"
-puts "T %d %d 8 32 1 1 0 4 1" % [1000 + (hlen + 1) * 500, 500 + (vlen + 1) * 500]
-puts "refdes=U?"
+if $sheet then
+    puts "T %d %d 8 32 0 0 0 4 1" % [1000 + (hlen + 1) * 500, 1500 + (vlen + 1) * 500]
+    puts "device=none"
+    puts "T 1000 %d 8 32 1 1 0 0 1" % [1100 + (vlen + 1) * 1000]
+    puts "refdes=S?"
+    puts "T 1000 900 8 16 1 1 0 2 1"
+    puts "source=schematic.sch"
+else
+    puts "T %d %d 8 32 1 1 0 4 1" % [1000 + (hlen + 1) * 500, 1500 + (vlen + 1) * 500]
+    puts "device=DEVICE"
+    puts "T %d %d 8 32 1 1 0 4 1" % [1000 + (hlen + 1) * 500, 500 + (vlen + 1) * 500]
+    puts "refdes=U?"
+end
 puts "B 1000 1000 %d %d 3 10 1 0 -1 -1 0 -1 -1 -1 -1 -1" % [(hlen + 1) * 1000, (vlen + 1) * 1000]
 
